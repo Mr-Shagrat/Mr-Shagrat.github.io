22048a38893f3a96879f306ea20d1449fa557cfe
Mr-Shagrat <cobhc900@gmail.com>
2021-08-21 13:59:02 +0300

    add response

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
+'use strict'
+
 const iconMenu = document.querySelector('.menu__burger');
+const menuBody = document.querySelector('.menu__middle');
 
-if (iconMenu) {
-  const menuBody = document.querySelector('.menu__nav');
+iconMenu.addEventListener("click", function (e) {
 
-  iconMenu.addEventListener("click", function (e) {
+  document.body.classList.toggle('lock');
+  iconMenu.classList.toggle('active');
+  menuBody.classList.toggle('active');
+});
 
-    document.body.classList.toggle('lock');
-    iconMenu.classList.toggle('active');
-    menuBody.classList.toggle('active');
 
-  });
+window.onresize = () => {
+  if (screen.width > 568) {
+    document.body.classList.remove('lock');
+    iconMenu.classList.remove('active');
+    menuBody.classList.remove('active');
+  }
 }
